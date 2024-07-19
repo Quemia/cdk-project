5a72ff48d8c811048ca0b9b2e6a3a20193ec0aab
Quemia-Oliveira <quemia@tartigrado.com>
2024-07-19 17:00:39 -0300

    add code-pipeline

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,48 +1,84 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda'
-import {aws_ec2} from "aws-cdk-lib";
-import {subnetId} from "aws-cdk-lib/aws-ec2/lib/util";
-
-const instanceName = 'hello-cdk';
-const instanceRoleArn = 'arn-role-instance'
-const az = 'us-east-2c';
-const amiName = 'nome da instancia';
-const subnetIds = ['num subnet'];
-
+import {aws_codebuild, aws_codecommit, aws_codepipeline, aws_codepipeline_actions, aws_ec2, aws_s3} from "aws-cdk-lib";
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // ec2 instance
-    const instance = new aws_ec2.Instance(this, 'Instance', {
-      instanceName,
-      instanceType: aws_ec2.InstanceType.of(
-          aws_ec2.InstanceClass.T2,
-          aws_ec2.InstanceSize.MICRO
-      ),
-      machineImage: aws_ec2.MachineImage.lookup({
-        name: amiName, // Nome da AMI (assumindo que 'amiName' é uma variável previamente definida)
+    const codeCommitRepository = new aws_codecommit.Repository(this, 'RepositoryCodeCommit',{
+      repositoryName: 'myAppPy',
+      description: 'my app made with python'
+    });
+
+    const CodeBuildProject = new aws_codebuild.Project(this, 'CodeCommitRepoBuild',{
+      // secondarySources: [
+      //     aws_codebuild.Source.codeCommit({
+      //       identifier: 'myAppPy',
+      //       repository: repo,
+      //     }),
+      // ],
+      source: aws_codebuild.Source.codeCommit({
+        identifier: 'myAppPy',
+        repository: codeCommitRepository,
       }),
-      vpc, // VPC onde a instância será criada
-      role, // função AMI associada a instância
-      vpcSubnets:{
-        subnets: subnetIds.map((subnetId, i) =>
-          aws_ec2.Subnet.fromSubnetAttributes(this, `Subnet${i}`, {
-            subnetId,
-            availabilityZone: az, // Zona de disponibilidade
-          })
-        )
-      },
-      securityGroup, //grupo de segurnaça associado
-      blockDevices:[
+    });
+
+
+    const CodeBucket = new aws_s3.Bucket(this, 'kekeBucketPy', {
+      versioned: true,
+    });
+
+
+    const pipelineSourceArtifact = new aws_codepipeline.Artifact();
+    const pipelineBuildArtifact = new aws_codepipeline.Artifact();
+
+    //
+    const pipelineSourceAction = new aws_codepipeline_actions.S3SourceAction({
+      actionName: 'S3Source',
+      bucket: CodeBucket,
+      bucketKey: 'path/to/source.txt',
+      output: pipelineSourceArtifact,
+    });
+
+    const pyPipeline = new aws_codepipeline.Pipeline(this, 'Pipeline', {
+      pipelineName: 'py-pipe',
+      artifactBucket,
+      stages: [
         {
-          deviceName: '/dev/xvda', // Nome do dispositivo para o volume
-          volume: aws_ec2.BlockDeviceVolume.ebs(30), // Volume EBS de 30 GB
+          stageName: 'Source',
+          actions: [
+              new aws_codepipeline_actions.CodeCommitSourceAction({
+              // CodeCommitSourceAction -> pipeline acionado sempre que tiver mudança no repositório do codecommit
+                actionName: 'Source',
+                repository: codeCommitRepository,
+                output: pipelineSourceArtifact,
+              }),
+          ],
+        },
+        {
+          stageName: 'Build',
+          actions: [
+              new aws_codepipeline_actions.CodeBuildAction({
+                actionName: 'Build',
+                input: pipelineSourceArtifact,
+                project: CodeBuildProject,
+                outputs: [pipelineBuildArtifact],
+              }),
+          ],
+        },
+        {
+          stageName: 'Deploy',
+          actions: [
+              new aws_codepipeline_actions.S3DeployAction({
+                actionName: 'Deploy',
+                input: pipelineSourceArtifact,
+                objectKey: `${pipelineSourceAction.variables.versionId}.txt`,
+                bucket: CodeBucket
+              }),
+          ],
         },
       ],
-      availabilityZone:az, // Zona de disponibilidade
     });
 
 
